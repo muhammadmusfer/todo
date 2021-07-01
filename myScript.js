f6d16c9bcbf293f7dd1bcd6d5737aa1a77920b2b
Muhammad Musfer <m.musfer@techno-soft.com>
2021-07-01 12:10:00 +0500

    Third Commit

--- myScript.js
+++ myScript.js
@@ -1,14 +1,19 @@
+
 function myfun(){
            
     var myvar = document.getElementById("inputField").value;
     var t = document.createTextNode(myvar);
     var listItem = document.createElement('li');
     listItem.appendChild(t);
+    t.className="text";
+    listItem.className="listItem";
+
     document.getElementById("list").appendChild(listItem);
 
     document.getElementById("inputField").value='';
     var button = document.createElement("button");
-    var txt = document.createTextNode("\u00D7");
+    var txt = document.createTextNode("DEL");
+   
     button.className = "close";
     button.appendChild(txt);
     listItem.appendChild(button);
@@ -16,7 +21,7 @@ function myfun(){
     for (i = 0; i < close.length; i++) {
          close[i].onclick = function() {
         var div = this.parentElement;
-        div.style.display = "none";
+        div.parentNode.removeChild(div);
     }
       
    }
